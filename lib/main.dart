367243802f291f087a0bad5fa6089154931ddd80
Pramod <pramodg2002@gmail.com>
2022-01-04 00:27:58 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
-import 'package:catalog/home_page.dart';
+import 'package:catalog/screens/home_page.dart';
+import 'package:catalog/screens/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Catalog());
 
 class Catalog extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => Homepage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
